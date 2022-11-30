8ce9fac03d6f33bbab81773c30600c72dfc44908
xnm-sdksdk <n.mendonca@protonmail.com>
2022-11-30 11:13:19 +0000

    imports all done

--- server.py
+++ server.py
@@ -1,2 +1,9 @@
+# used to send messages across the network they provide a form of inter process communication (IPC - mechanisms that an OS provides ti allow the processes to manage shared data)
+import socket
+# imports module of system
+import sys
+# imports time and date
+import time
+
 
 
