9af0181d50752a9977e326a0a59be970845c0524
xnm-sdksdk <n.mendonca@protonmail.com>
2022-11-30 11:29:58 +0000

    data flow

--- server.py
+++ server.py
@@ -6,6 +6,29 @@ import sys
 import time
 
 
+# SOCKET API CALLS AND DATA FLOW FOR TCP
+"""
+Server:
+socket
+bind
+listen
+accept
+recv
+send
+recv
+close
+
+Client:
+socket
+connect
+send
+recv
+close
+"""
+
+# 
+sckt = socket.socket()
+
 # gets the local host name of the device
 host = socket.gethostname()
 # print(host) prints the name of my machine
@@ -13,6 +36,19 @@ print("Serve will start on host: ", host)
 
 # makes the connection between devices; 8080 http alternative TCP UDP protocol 
 port = 8080
+
+
+# tuple
+sckt.bind((host,port))
+
+print("")
+
+print("Server done binding to host and port successfully")
+
+print("")
+
+# listen for our incoming connection, as a param (1) because we only want one connection to be established
+sckt.listen(1)
 
 
 
