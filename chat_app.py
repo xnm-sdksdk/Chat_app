f4301368b5c4c57dd4149a2beda22968c7cdeeaa
xnm-sdksdk <n.mendonca@protonmail.com>
2022-11-30 19:49:59 +0000

    connections

--- chat_app.py
+++ chat_app.py
@@ -1,13 +1,19 @@
-
+# used to send messages across the network they provide a form of inter process communication (IPC - mechanisms that an OS provides ti allow the processes to manage shared data)
 import socket
+# imports module of system
 import sys
+# imports time and date
 import time
 
+sckt = socket.socket()
+
+host = input("Enter host name of the server: ")
+port = 8080
 
 
+sckt.connect((host, port))
 
-
-
+print("Connected to chat server")
 
 
 
